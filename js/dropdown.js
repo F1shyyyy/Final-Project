e7fb6a4f3bf9a7fdb12becad57f347aa5f2ac99d
Ondřej Kábele <126025302+F1shyyyy@users.noreply.github.com>
2023-06-18 06:58:06 +0200

    Progress Report: No. 4 | Finished Forge site

---
+++ js/dropdown.js
@@ -0,0 +1,42 @@
+let forgeMods = document.getElementById("forge-modifikace");
+let verze = [5];
+verze[0] = document.getElementById("forge-mdp-1710");
+verze[1] = document.getElementById("forge-mdp-1102");
+verze[2] = document.getElementById("forge-mdp-1122");
+verze[3] = document.getElementById("forge-mdp-1165");
+verze[4] = document.getElementById("forge-mdp-1192");
+let pozice = 0;
+let zapnuto = false;
+function dropdownMods(){
+    if (!zapnuto) {
+        forgeMods.style.display = "flex";
+        zapnuto = true;
+    } else if (zapnuto) {
+        forgeMods.style.display = "none";
+        zapnuto = false;
+    }
+}
+function zmenaVerze(answer){
+    console.log(verze[pozice])
+    if (answer.value == 0) {
+        verze[pozice].style.display = "none";
+        verze[0].style.display = "flex";
+        pozice = 0;
+    } else if (answer.value == 1) {
+        verze[pozice].style.display = "none";
+        verze[1].style.display = "flex";
+        pozice = 1;
+    } else if (answer.value == 2) {
+        verze[pozice].style.display = "none";
+        verze[2].style.display = "flex";
+        pozice = 2;
+    } else if (answer.value == 3) {
+        verze[pozice].style.display = "none";
+        verze[3].style.display = "flex";
+        pozice = 3;
+    } else if (answer.value == 4) {
+        verze[pozice].style.display = "none";
+        verze[4].style.display = "flex";
+        pozice = 4;
+    }
+}
